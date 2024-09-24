85e3356a5bc03a8ec56c8bfe79ebeda8b0399050
Kicki Lindstrand <kicki.lindstrand@gmail.com>
2024-09-24 10:08:42 +0200

    reset and latest score

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-console.log('Hej');
-
 const homeScoreEl = document.querySelector('.scoreHome');
 const guestScoreEl = document.querySelector('.scoreGuest');
+const lastResultHome = document.querySelector('.resultHome');
+const lastResultGuest = document.querySelector('.resultGuest');
 let homeScore = 0;
 let guestScore = 0;
 
@@ -12,5 +12,25 @@ function addScore(element, score) {
   } else {
     guestScore += score;
     guestScoreEl.textContent = guestScore;
+  }
+}
+
+function resetScore() {
+  document.getElementById('home').classList.remove('winner');
+  document.getElementById('guest').classList.remove('winner');
+  lastResultHome.textContent = homeScore;
+  lastResultGuest.textContent = guestScore;
+  checkWinner();
+  homeScore = 0;
+  guestScore = 0;
+  homeScoreEl.textContent = homeScore;
+  guestScoreEl.textContent = guestScore;
+}
+
+function checkWinner() {
+  if (homeScore > guestScore) {
+    document.getElementById('home').classList.add('winner');
+  } else if (guestScore > homeScore) {
+    document.getElementById('guest').classList.add('winner');
   }
 }
